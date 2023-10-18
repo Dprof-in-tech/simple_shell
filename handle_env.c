4afff4b01b4acc30658dda04d8c884ab7d88384c
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-10-18 07:31:20 +0000

    handle_env

--- handle_env.c
+++ handle_env.c
@@ -7,16 +7,47 @@
  * Return: Always 0 success
  */
 
-int handle_env(char *command)
+void handle_env(char *command)
 {
-	char **env;
+	char **env, **env_copy = NULL;
+	int count = 0, i;
 
 	if (command != NULL)
 	{
 		for (env = environ; *env != NULL; env++)
 		{
-			printf("%s\n", *env);
+			count++;
 		}
+
+		env_copy = (char **)malloc((count + 1) * sizeof(char *));
+		if (env_copy == NULL)
+		{
+			perror("malloc");
+			return;
+		}
+
+		for (i = 0; i < count; i++)
+			env_copy[i] = environ[i];
+		env_copy[count] = NULL;
+
+		qsort(env_copy, count, sizeof(char *), cmpstr);
+
+		for (i = 0; i < count; i++)
+			printf("%s\n", env_copy[i]);
+
+		free(env_copy);
 	}
-	return (0);
+}
+
+/**
+ * cmpstr - Function to compare strings
+ * @b: input string
+ * @c: input_string
+ *
+ * Return: The compared string
+ */
+
+int cmpstr(const void *b, const void *c)
+{
+	return (strcmp(*(const char **)b, *(const char **)c));
 }
